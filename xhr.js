b02bad42ce5dd37c9cc0986a7dbe79a5b75d778e
alina <alinapetrus3@gmail.com>
2021-11-17 16:28:08 +0200

    123

--- xhr.js
+++ xhr.js
@@ -1,5 +1,5 @@
-// const requestURL = 'https://api.airtable.com/v0/appElurJfZ2WmdIUg/Furniture/?api_key=keys1gmUsZYDi7y0g'
-const requestURL = 'https://reliability.rebelmouse.dev/customfeeds/feed/ads-feed/json?include_sections=en/ads'
+const requestURL = 'https://api.airtable.com/v0/appElurJfZ2WmdIUg/Furniture/?api_key=keys1gmUsZYDi7y0g'
+// const requestURL = 'https://reliability.rebelmouse.dev/customfeeds/feed/ads-feed/json?include_sections=en/ads'
 const xhr = new XMLHttpRequest()
 
 xhr.open('GET', requestURL, true)

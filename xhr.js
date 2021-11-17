0aa1a8f3e0bc1c23e3046c06e092a5d20844b9e8
alina <alinapetrus3@gmail.com>
2021-11-17 18:48:31 +0200

    321

--- xhr.js
+++ xhr.js
@@ -1,9 +1,10 @@
-const requestURL = 'https://api.airtable.com/v0/appElurJfZ2WmdIUg/Furniture/?api_key=keys1gmUsZYDi7y0g'
-// const requestURL = 'https://reliability.rebelmouse.dev/customfeeds/feed/ads-feed/json?include_sections=en/ads'
+const requestURL = 'https://cors-anywhere.herokuapp.com/https://reliability.rebelmouse.dev/customfeeds/feed/ads-feed/json'
 const xhr = new XMLHttpRequest()
 
 xhr.open('GET', requestURL, true)
+
 xhr.onload = () => {
     console.log(JSON.parse(xhr.response))
 }
+
 xhr.send()
